c0e92b71788478cf99039d3172238962905b5ce2
david <davidlopezd860@gmail.com>
2020-11-09 17:19:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -357,7 +357,7 @@ def eliminar2(song):
   return jsonify({'message':'Se elimino la cancion'})
 
 if __name__=="__main__":
-  app.run(threaded=True, host="0.0.0.0",port=5000)
+  app.run(threaded=True,port=5000,debug=True)
 
 
 
